cf1fbe21affcfa62e558eaf5fb9b932fe7d02249
Riyyan Siddiqui <riyyan.s.24@gmail.com>
2024-10-05 17:01:53 +0500

    Added essential directories

---
+++ server/server.js
@@ -0,0 +1,22 @@
+// Filename - demoapp/app.js
+
+import express from "express";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Corrected path to serve static files from client/dist (no 'server' in path)
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Serve the React app for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
