abe8c3b56ce3be54aba6df1cbf83320c5a491d09
Riyyan Siddiqui <riyyan.s.24@gmail.com>
2024-10-13 13:35:17 +0500

    Added ShopPage and ProductById pages

--- server/server.js
+++ server/server.js
@@ -10,11 +10,15 @@ const PORT = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Corrected path to serve static files from client/dist (no 'server' in path)
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // Serve the React app for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/", "index.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.send("<h1>About Page<h1>");
 });
 
 app.listen(PORT, () => {
